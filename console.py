54e159cae3f2bd6de6b5a106f96dae4f7fd7e041
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2024-01-16 13:52:53 -0800

    test

--- console.py
+++ console.py
@@ -115,8 +115,6 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, args):
         """ Create an object of any class"""
-        def do_create(self, args):
-        """ Create an object of any class"""
         try:
             if not args:
                 raise SyntaxError()
@@ -216,9 +214,8 @@ class HBNBCommand(cmd.Cmd):
             if args not in HBNBCommand.classes:
                 print("** class doesn't exist **")
                 return
-            for k, v in storage._FileStorage__objects.items():
-                if k.split('.')[0] == args:
-                    print_list.append(str(v))
+            for k, v in storage.all(HBNBCommand.classes[args]).items():
+                print_list.append(str(v))
         else:
             for k, v in storage._FileStorage__objects.items():
                 print_list.append(str(v))
