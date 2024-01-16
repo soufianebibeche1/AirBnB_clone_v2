c0fd0461d533bf8020e7ac361886538828da16b4
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2024-01-16 13:46:42 -0800

    test

--- console.py
+++ console.py
@@ -115,23 +115,24 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, args):
         """ Create an object of any class"""
-        args = arg.split()
-        if not args:
+        def do_create(self, args):
+        """ Create an object of any class"""
+        try:
+            if not args:
+                raise SyntaxError()
+            arg_list = args.split(" ")
+            kw = {}
+            for arg in arg_list[1:]:
+                arg_splited = arg.split("=")
+                arg_splited[1] = eval(arg_splited[1])
+                if type(arg_splited[1]) is str:
+                    arg_splited[1] = arg_splited[1].replace("_", " ").replace('"', '\\"')
+                kw[arg_splited[0]] = arg_splited[1]
+        except SyntaxError:
             print("** class name missing **")
-            return
-        class_name = args[0]
-        if class_name not in classes:
+        except NameError:
             print("** class doesn't exist **")
-            return
-
-        # Extract parameters and values
-        param_values = {}
-        for param in args[1:]:
-            key, value = param.split('=')
-            param_values[key] = value.replace('_', ' ')
-
-        # Create a new instance with the specified parameters
-        new_instance = classes[class_name](**param_values)
+        new_instance = HBNBCommand.classes[arg_list[0]](**kw)
         new_instance.save()
         print(new_instance.id)
 
