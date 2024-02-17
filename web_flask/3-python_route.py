55787b378a6fdbcd9da8718af8e3c04403181ed4
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2024-02-17 20:54:28 +0000

    add

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+This module starts a Flask apllication.
+"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_hbnb():
+    """hello world"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def hbnb():
+    """add a path to the url"""
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def c_text(text):
+    """make a simple variable rule"""
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route('/python/<text>')
+def python_text(text="is cool"):
+    """give a rule a default value"""
+    return "Python {}".format(text.replace("_", " "))
+
+
+if __name__ == "__main__":
+    # values here are the default, mentioned as keepsake
+    app.run(host="0.0.0.0", port="5000")
