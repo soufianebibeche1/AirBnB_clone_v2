ce82f576c90d457909f10d649d0eef9cd375f8f0
soufianebibeche1 <soufiane.bibeche97@gmail.com>
2024-02-18 18:43:44 +0000

    test

--- models/amenity.py
+++ models/amenity.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 """This is the amenity class"""
 from models.base_model import BaseModel, Base
-from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String
-from models.place import place_amenity
+from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel, Base):
-   pass
+    """This is the class for Amenity
+    Attributes:
+        name: input name
+    """
+    __tablename__ = 'amenities'
+    name = Column(String(128), nullable=False)
